755f1b760fe506ef3411666f348fff98b6d4f4b3
Zack - Work Comp <zackmays@gmail.com>
2014-02-28 18:30:02 -0500

    Add edit route to posts controller

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -29,4 +29,28 @@ class PostsController extends AppController {
 			$this->Session->setFlash(__('Unable to add your post'));
 		}
 	}
+	
+	public function edit($id = null) {
+		if (!$id) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		
+		$post = $this->Post->findById($id);
+		if (!$post) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		
+		if ($this->request->is(array('post', 'put'))) {
+			$this->Post->id = $id;
+			if ($this->Post->save($this->request->data)) {
+				$this->Session-setFlash(__('Your post has been updated.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Session->setFlash(__('Unable to update your post'));
+		}
+		
+		if (!$this->request->data) {
+			$this->request->data = $post;
+		}
+	}
 }
--- app/View/Posts/view.ctp
+++ app/View/Posts/view.ctp
@@ -3,3 +3,5 @@
 <p><small>Created: <?php echo $post['Post']['created']; ?></small></p>
 
 <p><?php echo h($post['Post']['body']); ?></p>
+
+<p><?php echo $this->Html->link('Back', array('controller' => 'posts', 'action' => 'index')); ?></p>
